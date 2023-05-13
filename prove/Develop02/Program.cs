f4687168b011a2a0871b9345841c4412e15b697d
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-05-12 21:51:22 -0600

    Update Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        //Object of class Entry
+        Entry userChoice = new Entry();
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        while(userChoice._prompt != "5") 
+        {
+        Console.WriteLine("Please select one of the following choices:");
+
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+
+        Console.WriteLine("What would you like to do?");
+        userChoice._prompt = Console.ReadLine();
+        
+        }
+
+
+        
+        
     }
 }
