029cb16ae66e9af740eb94afce2364df0c5f4362
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-05-18 19:31:44 -0600

    Update PromptGenerator.cs

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -6,7 +6,8 @@ using System.Collections.Generic;
 public class PromptGenerator
 
 {
-    public  List<string> questions = new List <string>{
+    
+    public List<string> questions = new List <string>{
     "What was the most delicious food/dessert I ate today?",
     "What is one thing I learned today?",
     "What did I learn today from my personal scripture study?",
