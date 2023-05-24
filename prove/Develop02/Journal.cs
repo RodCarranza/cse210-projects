51f66da946c368bdf3750b9673ecd1be3493dae8
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-05-24 15:56:48 -0600

    Update Journal.cs

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -2,13 +2,20 @@ using System;
 using System.IO; //File
 using System.Collections.Generic;
 
+
 public class Journal
 
 
 { 
     public List<Entry> _entries =  new List<Entry>();
+    
+  public void AddEntry (Entry info)  {
 
+        _entries.Add(info);  
 
+    }
+
+    
     public void Display() 
     {
         foreach (Entry entry in _entries)
@@ -18,10 +25,27 @@ public class Journal
 
     }
 
+    // Saving to a File
+
+    public void SaveToFile(Entry info2) {
+
+        string fileName = "myFile.txt";
+
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            // You can use the $ and include variables just like with Console.WriteLine
+            
+            outputFile.WriteLine($"{info2}");
+        }
+
+    }
+
+
+}
+
     // Adding an entry
     // Displaying all the entries
     // The Journal display method could iterate through all Entry objects and call the Entry display method
     // Saving to a file *
     // Loading from a file *
 
-}
