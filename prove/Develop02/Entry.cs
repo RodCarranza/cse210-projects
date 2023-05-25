b1ed713feeb18a336c57da0513bb018f6d5cd1f3
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-05-24 20:51:10 -0600

    Update Entry.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -12,7 +12,7 @@ public class Entry
     public void Display()
     {
         Console.WriteLine($"Date: {_date} - Prompt: {_prompt}");
-        Console.WriteLine($"{_response}");
+        Console.WriteLine($"{_response}\n");
 
     }
 }
