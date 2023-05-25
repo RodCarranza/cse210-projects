9c2bd9be34ca83bf3d07d27c6d20534b2d5a3a4d
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-05-24 20:49:04 -0600

    Update PromptGenerator.cs

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Collections.Generic;
 
-
-
 public class PromptGenerator
-
 {
-    
     public List<string> questions = new List <string>{
     "What was the most delicious food/dessert I ate today?",
     "What is one thing I learned today?",
@@ -14,6 +10,27 @@ public class PromptGenerator
     "What was the weirdest thing about my day?",
     "If i had the chance to do something I was unable to do today, what would it be?",
     "How well did I do my job today?"};
+
+    public Entry PromptQuestion() {
+
+        Entry userInfo = new Entry();
+
+        // Date
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
+        userInfo._date = dateText;
+
+        // Random question
+        var random = new Random();
+        var list = questions;
+        int index = random.Next(list.Count);
+        userInfo._prompt = list[index];
+        Console.WriteLine(userInfo._prompt);
+
+        // Response
+        userInfo._response = Console.ReadLine();
+
+        return userInfo;
+    }
 }
 
-// More methods here.
