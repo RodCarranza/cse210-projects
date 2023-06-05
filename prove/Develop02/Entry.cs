1f80a2b3da349d5bcd3c8fccff06b1027d784fee
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-06-04 22:40:32 -0600

    Update Entry.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,16 +1,13 @@
 using System;
-
-
 public class Entry
 {
     // Hold data and have a display method. 
-    public string _prompt;
-    public string _response;
-    public string _date;
-
+    public string _date {get; set;}
+    public string _prompt {get; set;}
+    public string _response {get; set;}
+    
 public void Display()
     {
         Console.WriteLine($"Date: {_date} - Prompt: {_prompt}\n{_response}\n");
     }
 }
-
