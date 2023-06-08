1115129ff3c93ed80c349a35522ba1a05c10e3ac
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-06-08 00:12:58 -0600

    Create Word.cs

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Word {
+
+    // Every member variable should be private. Getters and Setters should be used to access the data.
+    // Getters should not have any parameters and should not prompt the user for anything, but instead, simply return a value.
+    // Setters should accept a single parameter and have a void return type.
+
+    /*
+    Word - Keeps track of a single word and whether it is shown or hidden. 
+    which has the responsibility for storing the text of a word and whether it is hidden or not,
+    */
+
+    
+}
