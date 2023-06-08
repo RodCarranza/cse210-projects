5d08e0e00cfeb30980967c648bccbe7b591bb961
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-06-08 00:11:26 -0600

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // The program can end in one of two ways: Either the user types quit, or all of the words in the scripture have been hidden.
+
+        Reference reference = new Reference("Juan", 1, 15);
+        
+        Scripture scripture = new Scripture(reference, "Esta es la escritura que debe aparecer en el programa.");
+
+        Console.WriteLine(scripture.GetRenderedText());
+
     }
 }
