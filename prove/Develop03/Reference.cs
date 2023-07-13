ff56972ef0c7f33c9353cecbf528f014c8ad0dc1
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-07-12 23:45:23 -0600

    Update Reference.cs

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,19 +1,13 @@
 using System;
 
 public class Reference {
-
+    /*
+    Reference - Keeps track of the book, chapter, and verse information.
+    */
     private string _book;
     private int _chapter;
     private int _verse;
     private int _endVerse;
-    // Every member variable should be private. Getters and Setters should be used to access the data.
-    // Getters should not have any parameters and should not prompt the user for anything, but instead, simply return a value.
-    // Setters should accept a single parameter and have a void return type.
-
-    /*
-    Reference - Keeps track of the book, chapter, and verse information.
-    ** At least 2 constructors in reference class
-    */
 
     //Constructor for single verse
     public Reference(string book, int chapter, int verse) {
@@ -29,7 +23,6 @@ public class Reference {
         _verse = verse;
         _endVerse = endVerse;
     }
-
     public string GetFormattedReference() {
 
         if(_endVerse > 0) {
@@ -40,6 +33,5 @@ public class Reference {
             string reference = $"{_book} {_chapter}:{_verse}";
             return reference;
         } 
-    }
-    
+    }  
 }
