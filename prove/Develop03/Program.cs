c789c7dc67f45f26d9fb5afd70694ad9fc435753
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-07-12 23:42:35 -0600

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,15 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.\n");
+        
+        //---------------------------------------------
+
+       scripture.GetRenderedText();
+
+       scripture.ShuffleList();
+
+       string response = " ";
+
         // The program can end in one of two ways: Either the user types quit, or all of the words in the scripture have been hidden.
 
-        Reference reference = new Reference("Juan", 1, 15);
+        while(scripture.IsCompletelyHidden()) {
+
+        Console.WriteLine("\nPress enter to continue or type quit to finish:");
+        response = Console.ReadLine();
         
-        Scripture scripture = new Scripture(reference, "This is the, scripture..");
-
-        Console.WriteLine(scripture.GetRenderedText());
-
-        //scripture.HideWords();
-
+        if(response == "quit") {
+            System.Environment.Exit(0);
+        }
+        else {
+            Console.Clear();
+            scripture.HideWords();
+            scripture.GetRenderedText();
+        }
+       
+       }
     }
 }
+
