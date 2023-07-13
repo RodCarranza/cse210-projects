ef743ca27a29f45fdd892f1f9c29f7b9ede2a605
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-07-12 23:44:27 -0600

    Update Word.cs

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,48 +1,32 @@
 using System;
 
 public class Word {
-
-    private string _words;
-
-    // Every member variable should be private. Getters and Setters should be used to access the data.
-    // Getters should not have any parameters and should not prompt the user for anything, but instead, simply return a value.
-    // Setters should accept a single parameter and have a void return type.
-
     /*
-    Word - Keeps track of a single word and whether it is shown or hidden. 
-    which has the responsibility for storing the text of a word and whether it is hidden or not,
+    Keeps track of single words and initialize their state (hidden or shown).
     */
-
-    //Constructor
-
-    /* 
-     In addition, the constructor will need to set the initial visibility of the word 
-    (whether it is shown or hidden). Is this something that the user should pass in directly, or can the constructor just assign a value?
-
-    */
+    private string _words;
+    private bool _isHidden;
 
     public Word(string words) {
-        _words = words;   
-
+        _words = words;
+        _isHidden = false;
     }
-
     //Methods
-
     public void Hide() {
+        _isHidden = true;  
+    }
+    public bool IsHidden() {
+        return _isHidden; 
 
     }
+    public string GetRenderedText() {
+        if(_isHidden) {
+            string hiddenWords = new String('_', _words.Length);
+            return hiddenWords + " ";
 
-    public void Show() {
+        } else {
+            return _words + " "; 
 
+        }
     }
-
-    public void IsHidden() {
-
-    }
-
-    public void GetRenderedText() {
-        //Could be string instead of void
-    }
-
 }
-
