60ada9e1e94f6e62108872e6f427811cbe0a46be
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-07-13 22:48:11 -0600

    Update Word.cs

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -2,11 +2,10 @@ using System;
 
 public class Word {
     /*
-    Keeps track of single words and initialize their state (hidden or shown).
+    Keeps track of single words and initializes their state (hidden or shown).
     */
     private string _words;
     private bool _isHidden;
-
     public Word(string words) {
         _words = words;
         _isHidden = false;
@@ -23,10 +22,11 @@ public class Word {
         if(_isHidden) {
             string hiddenWords = new String('_', _words.Length);
             return hiddenWords + " ";
-
         } else {
             return _words + " "; 
 
         }
     }
 }
+
+
