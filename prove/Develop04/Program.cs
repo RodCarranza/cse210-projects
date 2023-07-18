ebd26e0cb8f2ed4e999d81056bf8fad088024619
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-07-18 00:08:46 -0600

    Update Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -3,7 +3,51 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Develop04 World!");
+        string choice = "";
+
+        // Menu
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine(" 1. Start breathing activity");
+        Console.WriteLine(" 2. Start reflecting activity");
+        Console.WriteLine(" 3. Start listing activity");
+        Console.WriteLine(" 4. Quit");
+        Console.Write("Select a choice from the menu: ");
+
+        choice = Console.ReadLine();
+
+        if(choice == "1") {
+            Console.Clear();
+            BreathingActivity breathing = new BreathingActivity("Breathing",
+            "relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
+            breathing.DisplayStartingMsg();
+            breathing.PauseWhileSpinner();
+            breathing.Breathing();
+            breathing.DisplayEndingMsg();
+            
+
+        } else if (choice == "2") {
+            Console.Clear();
+            ReflectingActivity reflecting = new ReflectingActivity("Reflecting",
+            "reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life."); 
+            reflecting.DisplayStartingMsg();
+
+
+        } else if (choice == "3") {
+
+            Console.Clear();
+            ListingActivity listing = new ListingActivity("Listing",
+            "reflect on the good things in your life by having you list as many things as you can in a certain area."); 
+            listing.DisplayStartingMsg();
+
+
+        } else {
+            System.Environment.Exit(0);
+
+        }   
     }
+
 }
+    
+
