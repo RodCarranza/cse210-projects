c38d4aab2dbc7222b7586e5e9f1d5774411cdfda
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-07-18 00:10:24 -0600

    Create ReflectingActivity.cs

---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class ReflectingActivity: Activity
+{
+    public ReflectingActivity(string activityName, string description) 
+    : base (activityName, description)
+    {
+
+
+    }
+
+}
