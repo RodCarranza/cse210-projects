4f5c0cc092550b00c3e96ebd62bb032d38fd6cf0
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-07-19 00:35:27 -0600

    Update ListingActivity.cs

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -2,12 +2,50 @@ using System;
 
 public class ListingActivity: Activity
 {
-
+    private List <string> _questionsList = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+    private List <string> _userInput = new List<string>();
     public ListingActivity(string activityName, string description) 
     : base (activityName, description)
     {
 
-
     }
 
+    public void DisplayQuestion()
+    {
+        var random = new Random();
+        var list = _questionsList;
+        int index = random.Next(list.Count);
+
+        Console.WriteLine("List as many responses you can to the following prompt:");
+        Console.WriteLine($"--- {list[index]} ---");
+        Console.Write($"You may begin in: ");
+        PauseWhileCountdown(5);
+        Console.WriteLine();
+    }
+
+    public void SaveAnswers()
+    {
+        // Set the time of the listing activity
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(GetDurationSec());
+        string answer = "";
+        
+        while(DateTime.Now < endTime)
+        {
+            _userInput.Add(answer);
+            answer = Console.ReadLine();
+        } 
+    }
+
+    public void DisplayItemsTotal()
+    {
+        Console.WriteLine($"You listed {_userInput.Count} items!");
+    }
 }
