e42a8ac0c9078315d0d886c2782014655adc5369
Rodrigo Carranza <93809998+RodCarranza@users.noreply.github.com>
2023-07-18 20:00:04 -0600

    Update Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -32,6 +32,9 @@ class Program
             ReflectingActivity reflecting = new ReflectingActivity("Reflecting",
             "reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life."); 
             reflecting.DisplayStartingMsg();
+            reflecting.PauseWhileSpinner();
+            reflecting.DisplayPrompt();
+            reflecting.DisplayPromptQuestions();
 
 
         } else if (choice == "3") {
@@ -40,6 +43,7 @@ class Program
             ListingActivity listing = new ListingActivity("Listing",
             "reflect on the good things in your life by having you list as many things as you can in a certain area."); 
             listing.DisplayStartingMsg();
+            listing.PauseWhileSpinner();
 
 
         } else {
